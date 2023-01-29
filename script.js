3d77a3843ce869b385091f699a60c02b85bdbe61
LiviaCCO <livia.ccapistrano@gmail.com>
2023-01-29 14:39:56 -0300

    versao todas as telas

--- script.js
+++ script.js
@@ -485,6 +485,10 @@ function verifyUserQuizz(){
         setInitialPage12();
     }
 }
+
+verifyUserQuizz();
+
+/* Definindo variáveis - Tela 2 */
 let idQuizz;
 let quizzChosen={};
 let ans={};
@@ -679,116 +683,3 @@ function screen2(){
     getQuizz();
 }
     
-
-/* Escolhendo a alternativa */
-function optionChosen(selected){
-    /* para impedir a mudança de resposta */
-    if(selected.classList.value === "option"){
-        const options = selected.parentNode.querySelectorAll(".option");
-        /* para mostrar gaarito ao clicar */
-        for (let i=0; i<options.length; i++){
-            options[i].classList.add('notChosen');
-            
-            /* Mostrando o gabarito */
-            if (options[i].id === "true"){
-                options[i].classList.add('right');
-
-            }
-            else{
-                options[i].classList.add('wrong');
-            }
-        }
-        selected.classList.remove('notChosen')
-
-        /* contabilizando os pontos */
-        console.log(selected.id);
-        if(selected.id === "true"){
-            score+=1;
-        }
-
-        /* Para scrollar para o próximo box */
-        selected.parentNode.parentNode.classList.remove('void');
-        const next = document.querySelector(".void");
-        if(next === null){
-            finalResult();
-        }
-        /* scrollar a página após 2 segundos */
-        /* função setTimeout */
-        else{
-            function scroll(){
-                next.scrollIntoView({behavior:"smooth"}); 
-            }
-            setTimeout(scroll, 2000);
-        }
-    }    
-}
-/* Função para embaralhar as respostas */
-function comparador() { 
-    return Math.random() - 0.5; 
-}
-
-/* Abrindo o Quizz */
-function starting(resposta){
-    quizzChosen=resposta.data;
-    /* para guardar o array de níveis de acerto */
-    levels = quizzChosen.levels;
-    /* Para lançar as questões do quizz */
-    let questionsQuizz="";
-    numQuestions = quizzChosen.questions.length;
-    for(let i=0; i<numQuestions; i++){
-        
-        /* Para lançar as opções de cada questão do quizz */
-        let ans = quizzChosen.questions[i].answers;
-        console.log(ans);
-
-        //para embaralhar as respostas
-        const newAns = ans.sort(comparador); 
-
-        console.log(newAns);
-        let options="";
-        for (let j=0; j<ans.length; j++){
-
-            options +=`<div onclick="optionChosen(this)" id="${newAns[j].isCorrectAnswer}" class="option">
-                    <img src=${newAns[j].image}>
-                    <div class="nameOption">${newAns[j].text}</div>
-                </div>
-            `
-        }
-
-        questionsQuizz += `<div class="questionBox void">
-            <div style="background-color:${quizzChosen.questions[i].color};" class="questionTitle">${quizzChosen.questions[i].title}</div>
-            <div class="options"> 
-            ${options}
-            </div>
-        </div>`
-    }
-    /* Alterando a imagem do Quizz */
-    
-    /* const page2 = document.querySelector(".page2"); */
-    page2.innerHTML = `<div style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6)), url(${quizzChosen.image});" class="quizzName">${quizzChosen.title}</div>
-        ${questionsQuizz}
-    `    
-}
-
-/* Função para o caso de erro */
-function error(resposta){
-    alert ("Tente novamente mais tarde!")
-    console.log(resposta);
-}
-
-/* função para distribuir as questoes */
-function getQuizz(){
-    const promisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`);
-    promisse.then(starting);
-    promisse.catch(error);
-}
-
-/* Para obter o ID do Quizz */
-function screen2(){
-    idQuizz  = prompt("Qual o Id do Quizz?");
-    getQuizz();
-}
-    
-
-
-verifyUserQuizz();
